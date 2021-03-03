281e2d8c3dbcb68b344f2840747244186917cfc6
Ivan Klevets <ivan.klevets@gmail.com>
2021-03-03 08:41:09 +0200

    cba swiper

--- cba/js/main.js
+++ cba/js/main.js
@@ -49,3 +49,18 @@ $(window).scroll(function () {
       .addClass('active');
   }
 });
+
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+  
